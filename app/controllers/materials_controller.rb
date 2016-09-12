4001f20263ed1e93ad94f4993029ac0cdc5ad1b5
JesseYang <jesse.yang1985@gmail.com>
2016-09-12 22:48:16 +0800

    update

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,9 +2,8 @@ class MaterialsController < ApplicationController
 
   def create
     image = Image.new
-    image.image = params[:wangEditorH5File]
-    filetype = "png"
-    image.store_image!
+    image.image = params[:wangEditorPasteFile]
+    ret = image.store_image!
     filepath = image.image.file.file
     render text: "/uploads/images/" + filepath.split('/')[-1] and return
   end
